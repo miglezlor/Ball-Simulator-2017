8c7dd0530b5b6cb9b57eed2d75cfd0a8a493a8ed
miglezlor <mgonzalezlorenzo@danielcastelao.org>
2017-03-29 01:01:45 +0200

    Juego terminado

---
+++ Ball Simulator 2017/Assets/Scripts/CamaraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraControl : MonoBehaviour {
+
+    public GameObject pelota;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - pelota.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = pelota.transform.position + offset;
+    }
+}
---
+++ Ball Simulator 2017/Assets/Scripts/PelotaControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PelotaControl : MonoBehaviour {
+
+    public float velocidad;
+    public Text textoCont;
+    public Text textoVict;
+
+    private Rigidbody rb;
+    private int contador;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        contador = 0;
+        UpdContador();
+        textoVict.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float movHor = Input.GetAxis("Horizontal");
+        float movVer = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movHor, 0.0f, movVer);
+
+        rb.AddForce(movimiento * velocidad);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coleccionable"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 1;
+            UpdContador();
+        }
+    }
+
+    void UpdContador()
+    {
+        textoCont.text = "Coleccionables: " + contador.ToString();
+        if(contador>= 12)
+        {
+            textoVict.text = "HAS GANADO!";
+        }
+    }
+}
